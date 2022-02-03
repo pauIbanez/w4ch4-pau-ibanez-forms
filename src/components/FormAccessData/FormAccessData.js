2ebbb02cea414941ef5ab3d01a38c61d73dd24a5
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-03 20:29:23 +0100

    Added logic to check if repeated password is correct

--- src/components/FormAccessData/FormAccessData.js
+++ src/components/FormAccessData/FormAccessData.js
@@ -1,36 +1,68 @@
-const FormAccesData = ({ formData: { username, password }, onIputChange }) => {
+import { useRef, useState } from "react";
+
+const FormAccesData = ({ formData: { username, password }, onInputChange }) => {
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const repeatedPasswordCheck = useRef(false);
+
+  const validatePassword = (ref, inputedPassword) => {
+    switch (ref) {
+      case "password":
+        if (inputedPassword === repeatPassword) {
+          repeatedPasswordCheck.current = true;
+        } else {
+          repeatedPasswordCheck.current = false;
+        }
+        break;
+
+      case "repeatPassword":
+        if (inputedPassword === password) {
+          repeatedPasswordCheck.current = true;
+        } else {
+          repeatedPasswordCheck.current = false;
+        }
+        setRepeatPassword(inputedPassword);
+        break;
+
+      default:
+    }
+  };
+
   return (
     <>
       <input
         type="text"
         value={username}
         onChange={(event) => {
-          onIputChange(event.target.name, event.target.value);
+          onInputChange(event.target.name, event.target.value);
         }}
-        placeholder="Username"
+        placeholder="Write a username"
         name="username"
         className="form__input"
       />
       <input
-        type="text"
-        value={username}
+        type="password"
+        value={password}
         onChange={(event) => {
-          onIputChange(event.target.name, event.target.value);
+          onInputChange(event.target.name, event.target.value);
+          validatePassword(event.target.name, event.target.value);
         }}
-        placeholder="Username"
-        name="username"
+        placeholder="Write a password"
+        name="password"
         className="form__input"
       />
-      <input
-        type="text"
-        value={username}
-        onChange={(event) => {
-          onIputChange(event.target.name, event.target.value);
-        }}
-        placeholder="Username"
-        name="username"
-        className="form__input"
-      />
+      <div className="form__repeat-password">
+        <input
+          type="password"
+          value={repeatPassword}
+          onChange={(event) => {
+            validatePassword(event.target.name, event.target.value);
+          }}
+          placeholder="Write the password again"
+          name="repeatPassword"
+          className="form__input"
+        />
+        {repeatPassword ? (repeatedPasswordCheck.current ? "yes" : "no") : ""}
+      </div>
     </>
   );
 };
